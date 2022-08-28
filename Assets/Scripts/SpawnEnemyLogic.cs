2adf091cb893d12a49ee62063ee4b5a77cfc40b9
TerentevSA <mrsam002@yandex.ru>
2022-08-28 07:53:26 +0300

    ex6

---
+++ Assets/Scripts/SpawnEnemyLogic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyLogic : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private float _timeToSpawn;
+
+    private GameObject[] _spawnPoints;
+    private string _spawnPointTag = "SpawnPoint";
+
+    private void Start()
+    {
+        _spawnPoints = GameObject.FindGameObjectsWithTag(_spawnPointTag);
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        int i = 0;
+
+        for(; ; )
+        {
+            Instantiate(_enemy, _spawnPoints[i].transform.position, _spawnPoints[i].transform.rotation);
+
+            if(i + 1 == _spawnPoints.Length)
+            {
+                i = 0;
+            }
+            else
+            {
+                i++;
+            }
+
+            yield return new WaitForSeconds( _timeToSpawn );
+        }
+    }
+}
