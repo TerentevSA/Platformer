ae8b3a55b45df46b5e1d29170e8b81ea2a8d50b9
TerentevSA <mrsam002@yandex.ru>
2022-09-02 11:43:15 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float _minGroundNormalY;
+    [SerializeField] private float _gravity;
     [SerializeField] private float _speed;
+    [SerializeField] private LayerMask _layerMask;
 
+    private Vector2 _direction;
+    private Vector2 _normal;
     private Rigidbody2D _rb;
-    private Vector2 _velocity;
+    private List<RaycastHit2D> _raycastHit;
+    private ContactFilter2D _contactFilter;
+    private bool _isGrounded;
+    private bool _canSecondJump;
+    private float _distance;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _contactFilter.useTriggers = false;
+        _contactFilter.SetLayerMask(_layerMask);
+        _contactFilter.useLayerMask = true;
+        _canSecondJump = true;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        _velocity = new Vector2(_speed * Input.GetAxisRaw("Horizontal") * Time.deltaTime, 0);
+        _isGrounded = false;
+        _direction.x += Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime;
+        _distance = _direction.magnitude;
     }
 }
